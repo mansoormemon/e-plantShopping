233534a6b5d0f300d5f074d4e3d01a3ca30b6d91
Mansoor Ahmed Memon <mansoormemon@barakah.works>
2025-12-01 17:03:11 -0500

    task 1

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,12 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
     },
     removeItem: (state, action) => {
     },
     updateQuantity: (state, action) => {
 
-    
+
     },
   },
 });
